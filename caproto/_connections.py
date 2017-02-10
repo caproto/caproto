619cb77143a7d4b970b4b0c0a89f465542cd4fdd
danielballan <daniel.b.allan@gmail.com>
2017-02-10 14:30:04 -0500

    Fix up convenience methods; docstrings galore.

--- caproto/_connections.py
+++ caproto/_connections.py
@@ -13,7 +13,20 @@ from ._utils import *
 __all__ = ['VirtualCircuit', 'Connections', 'Channel']
 
 
+DEFAULT_PROTOCOL_VERSION = 13
+
+
 class VirtualCircuit:
+    """
+    An object encapulating the state of one CA client--server connection.
+
+    This object can be created as soon as we know the address ``(host, port))``
+    of our peer (client/server depending on our role).
+
+    It is a companion to a TCP socket managed by the user. All data
+    received over the socket should be passed to :meth:`recv`. Any data sent
+    over the socket should first be passed through :meth:`send`.
+    """
     def __init__(self, address, priority):
         self.our_role = CLIENT
         self.their_role = SERVER
@@ -24,6 +37,12 @@ class VirtualCircuit:
         self._channels_cid = {}
         self._channels_sid = {}
         self._ioids = {}
+        # This is only used by the convenience methods, to auto-generate ioid.
+        self._ioid_counter = itertools.count(0)
+
+    def new_ioid(self):
+        # TODO Be more clever and reuse abandoned ioids; avoid overrunning.
+        return next(self._ioid_counter)
 
     def add_channel(self, channel):
         self._channels_cid[channel.cid] = channel
@@ -71,15 +90,14 @@ class VirtualCircuit:
         return command
 
 
-class Connections:
-    """An object encapsulating the state of Channel Access connections.
+class Hub:
+    """An object encapsulating the state of CA connections in process.
 
-    This tracks the state of:
-    - one Client and all its connected Servers,
-    - or one Server and all its connected Clients
+    This tracks the state of one Client and all its connected Servers or one
+    Server and all its connected Clients.
 
     It sees all outgoing bytes before they are sent over a socket and receives
-    all incoming bytes after they are received from a socket. it verifies that
+    all incoming bytes after they are received from a socket. It verifies that
     all incoming and outgoing commands abide by the Channel Access protocol,
     and it updates an internal state machine representing the state of all
     CA channels and CA virtual circuits.
@@ -87,7 +105,6 @@ class Connections:
     It may also be used to compose valid commands using a pleasant Python API
     and to decode incomming bytestreams into these same kinds of objects.
     """
-    PROTOCOL_VERSION = 13
 
     def __init__(self, our_role):
         if our_role not in (SERVER, CLIENT):
@@ -97,15 +114,33 @@ class Connections:
             self.their_role = SERVER
         else:
             self.their_role = CLIENT
-        self._names = {}  # map known names to (host, port)
-        self._circuits = {}  # keyed by (address, priority)
+        self._names = {}  # map known Channel names to (host, port)
+        self._circuits = {}  # keyed by ((host, port), priority)
         self._channels = {}  # map cid to Channel
+        self._datagram_inbox = deque()  # datagrams to be parsed into Commands
+        self._parsed_commands = deque()  # parsed Commands to be processed
+        # This is only used by the convenience methods, to auto-generate a cid.
         self._cid_counter = itertools.count(0)
-        self._datagram_inbox = deque()
-        self._parsed_commands = deque()
+
+    def new_cid(self):
+        # TODO Be more clever and reuse abandoned cids; avoid overrunning.
+        return next(self._cid_counter)
 
     def new_channel(self, name, priority=0):
-        cid = next(self._cid_counter)
+        """
+        A convenience method: instantiate a new :class:`Channel`.
+
+        You are not required to use this method; you can also role your own
+        :class:`Channel`. The Hub state will not be updated until a
+        :class:`SearchResponse` for this :class:`Channel` is processed.
+
+        This is equivalent to:
+
+        ``Channel(<Hub>, None, <UNIQUE_INTEGER>, name, priority)``
+        """
+        # This method does not change any state other than the cid counter,
+        # which is neither important nor coupled to anything else.
+        cid = self.new_cid()
         circuit = None
         channel = Channel(name, circuit, cid, name, priority)
         self._channels[cid] = channel
@@ -113,7 +148,6 @@ class Connections:
         # host, skip the Search step and create a circuit.
         # if name in self._names:
         #     circuit = self._circuits[(self._names[name], priority)]
-        msg = SearchRequest(name, cid, self.PROTOCOL_VERSION)
         return channel
 
     def send_broadcast(self, command):
@@ -163,18 +197,25 @@ class Connections:
 
 
 class Channel:
-    "An object encapsulating the state of the EPICS Channel on a Client."
-    def __init__(self, client, circuit, cid, name, priority=0):
-        self._cli = client
-        self._circuit = circuit
+    """An object encapsulating the state of the EPICS Channel on a Client.
+    
+    A Channel may be created as soon as the desired ``name`` is known, maybe
+    before the server providing that name is located.
+
+    A Channel will be assigned to a VirtualCircuit (corresponding to one
+    client--server TCP connection), which is may share with other Channels.
+    """
+    def __init__(self, hub, circuit, cid, name, priority=0):
+        self._hub = hub
+        self._circuit = circuit  # may be None at __init__ time
         self.cid = cid
         self.name = name
-        self.priority = priority
+        self.priority = priority  # on [0, 99]
         self._state = ChannelState()
+        # These are updated when the circuit processes CreateChanResponse.
         self.native_data_type = None
         self.native_data_count = None
         self.sid = None
-        self._requests = deque()
 
     @property
     def circuit(self):
@@ -182,6 +223,9 @@ class Channel:
 
     @circuit.setter
     def circuit(self, circuit):
+        # The hub assigns a VirtualCircuit to this Channel.
+        # This occurs when a :class:`SearchResponse` locating the Channel's
+        # name is processed.
         if self._circuit is None:
             self._circuit = circuit
             circuit.add_channel(self)
@@ -189,15 +233,47 @@ class Channel:
         else:
             raise RuntimeError("circuit may only be set once")
 
+    def search(self):
+        """
+        A convenience method: generate a valid :class:`SearchRequest`.
+
+        Returns
+        -------
+        circuit, SearchRequest
+        """
+        return SearchRequest(self.name, self.cid, DEFAULT_PROTOCOL_VERSION)
+
     def read(self, data_type=None, data_count=None):
+        """
+        A convenience method: generate a valid :class:`ReadNotifyRequest`.
+
+        Returns
+        -------
+        circuit, ReadNotifyRequest
+        """
         if data_type is None:
             data_type = self.native_data_type
         if data_count is None:
             data_count = self.native_data_count
-        return self.circuit, ReadRequest(...)
+        ioid = self.circuit.new_ioid()
+        return self.circuit, ReadNotifyRequest(data_type, data_count,
+                                               self.sid, ioid)
 
     def write(self, data):
-        return self.circuit, WriteRequest(...)
+        """
+        A convenience method: generate a valid :class:`WriteNotifyRequest`.
+
+        Parameters
+        ----------
+        data : object
+
+        Returns
+        -------
+        circuit, ReadNotifyRequest
+        """
+        ioid = self.circuit.new_ioid()
+        return self.circuit, ReadNotifyRequest(data, data_type, data_count,
+                                               self.sid, ioid)
 
     def subscribe(self, data_type=None, data_count=None):
         if data_type is None:
