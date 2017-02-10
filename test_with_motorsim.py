aeea377df4b02108b315ff346f96a1c1a359e7bd
danielballan <daniel.b.allan@gmail.com>
2017-02-09 21:03:59 -0500

    Add a SearchRequest to the motor test.

--- test_with_motorsim.py
+++ test_with_motorsim.py
@@ -3,7 +3,7 @@ import socket
 
 
 CA_REPEATER_PORT = 5065
-pv = "XF:31IDA-OP{Tbl-Ax:X1}Mtr"
+pv1 = "XF:31IDA-OP{Tbl-Ax:X1}Mtr"
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -12,7 +12,8 @@ send_bcast = lambda msg: sock.sendto(bytes(msg), ('', CA_REPEATER_PORT))
 recv_bcast = lambda: sock.recvfrom(4096)
 
 # Send data
-ip = socket.gethostbyname(socket.gethostname())
+# ip = socket.gethostbyname(socket.gethostname())
+ip = '127.0.0.1'
 reg_command = ca.RepeaterRegisterRequest(ip)
 print("Sending", reg_command)
 send_bcast(reg_command)
@@ -21,3 +22,14 @@ send_bcast(reg_command)
 print('waiting to receive')
 data, address = recv_bcast()
 print('received "%s"' % data)
+
+
+cli = ca.Connections(our_role=ca.CLIENT)
+chan1 = cli.new_channel(pv1)
+print('searching for %s' % pv1)
+bytes_to_send = cli.send_broadcast(ca.SearchRequest(pv1, 0, 13))
+send_bcast(bytes_to_send)
+bytes_received, address = recv_bcast()
+cli.recv_broadcast(bytes_received, address)
+command = cli.next_command()
+print('received', command)
