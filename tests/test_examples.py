15917f137b6e01947c08103f6463227c5cf7231c
danielballan <daniel.b.allan@gmail.com>
2017-03-19 16:45:34 -0400

    TST: Test server.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,6 +2,7 @@ import signal
 import os
 import time
 from multiprocessing import Process
+import curio
 
 
 def test_synchronous_client():
@@ -23,3 +24,48 @@ def test_synchronous_client():
 def test_curio_client():
     from caproto.examples.curio_client import main
     main()
+
+
+def test_curio_server(kernel):
+    import caproto.examples.curio_server as server
+    import caproto.examples.curio_client as client
+
+    async def run_server():
+        pvdb = ["pi"]
+        ctx = server.Context('0.0.0.0', 5064, pvdb)
+        await ctx.run()
+
+    async def run_client():
+        # Some user function to call when subscriptions receive data.
+        called = []
+        def user_callback(command):
+            print("Subscription has received data.")
+            called.append(True)
+
+        ctx = client.Context()
+        await ctx.register()
+        await ctx.search('pi')
+        chan1.register_user_callback(user_callback)
+        # ...and then wait for all the responses.
+        await chan1.wait_for_connection()
+        reading = await chan1.read()
+        print('reading:', reading)
+        await chan1.subscribe()
+        await chan1.unsubscribe(0)
+        await chan1.write((5,))
+        reading = await chan1.read()
+        print('reading:', reading)
+        await chan1.write((6,))
+        reading = await chan1.read()
+        print('reading:', reading)
+        await chan1.clear()
+        assert called
+
+    async def task():
+        server_task = await curio.spawn(run_server())
+        await curio.sleep(5)
+        client_task = await curio.spawn(run_client())
+        await client_task.join()
+        await kernel_task.kill()
+
+    curio.run(task())
