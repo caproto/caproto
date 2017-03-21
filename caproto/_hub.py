165467a1b0677ccbfc6a37127d2d1b46cec16d3d
K Lauer <kenneth.lauer@gmail.com>
2017-03-20 21:38:17 -0700

    MNT: add metadata to read

--- caproto/_hub.py
+++ caproto/_hub.py
@@ -856,7 +856,8 @@ class ServerChannel(_BaseChannel):
         command = ClearChannelResponse(self.sid, self.cid)
         return command
 
-    def read(self, values, ioid, data_type=None, data_count=None, status=1):
+    def read(self, values, ioid, data_type=None, data_count=None, status=1, *,
+             metadata=None):
         """
         Generate a valid :class:`ReadNotifyResponse`.
 
@@ -874,6 +875,8 @@ class ServerChannel(_BaseChannel):
             :attr:`native_data_count`.
         status : integer, optional
             Default is 1 (success).
+        metadata :
+            Status and control metadata for the values
 
         Returns
         -------
@@ -881,7 +884,7 @@ class ServerChannel(_BaseChannel):
         """
         data_type, data_count = self._fill_defaults(data_type, data_count)
         command = ReadNotifyResponse(values, data_type, data_count, status,
-                                     ioid)
+                                     ioid, metadata=metadata)
         return command
 
     def write(self, ioid, data_type=None, data_count=None, status=1):
