b312432ea86eb1cd9ea8ad4002e74f10298e1f9f
Dan Allan <dallan@bnl.gov>
2017-03-23 18:22:53 +0000

    FIX: Missing socket option in curio client example.

--- caproto/examples/curio_client.py
+++ caproto/examples/curio_client.py
@@ -211,7 +211,7 @@ class Context:
         # UDP socket broadcasting to CA servers
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
                              socket.IPPROTO_UDP)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.udp_sock = sock
 
         self.registered = False  # refers to RepeaterRegisterRequest
