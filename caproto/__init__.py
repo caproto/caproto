d8665f001bffeeee2359ef294aae7781c867b843
Thomas A Caswell <tcaswell@bnl.gov>
2017-09-19 23:08:36 -0400

    MNT: hide import from _data to support py3.4

--- caproto/__init__.py
+++ caproto/__init__.py
@@ -6,7 +6,10 @@ from ._state import *
 from ._commands import *
 from ._dbr import *
 from ._status import *
-from ._data import *
+try:
+    from ._data import *
+except SyntaxError:
+    pass
 
 from ._version import get_versions
 __version__ = get_versions()['version']
