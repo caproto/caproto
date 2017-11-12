36cc4b1aa7d8d9a07888fba7fade4b536376871b
K Lauer <kenneth.lauer@gmail.com>
2017-11-11 16:10:45 -0800

    TST/BENCH: add start of many connections test (curio fails)

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -21,11 +21,21 @@ def setup_module():
     logging.getLogger('benchmarks.util').setLevel('DEBUG')
     logging.getLogger('caproto').setLevel('INFO')
 
-    db_text = ca.benchmarking.make_database(
-        {('wfioc:wf{}'.format(sz), 'waveform'): dict(FTVL='LONG', NELM=sz)
-         for sz in (4000, 8000, 50000, 1000000)
-         },
-    )
+    waveform_db = {
+        ('wfioc:wf{}'.format(sz), 'waveform'): dict(FTVL='LONG', NELM=sz)
+        for sz in (4000, 8000, 50000, 1000000)
+    }
+
+    many_connections_db = {
+        ('connections:{}'.format(i), 'ao'): dict(VAL=i)
+        for i in range(1000)
+    }
+
+    full_db = dict()
+    full_db.update(many_connections_db)
+    full_db.update(waveform_db)
+
+    db_text = ca.benchmarking.make_database(full_db)
 
     ioc_handler = ca.benchmarking.IocHandler(logger=logger)
     ioc_handler.setup_ioc(db_text=db_text, max_array_bytes='10000000')
@@ -144,4 +154,127 @@ def test_waveform_get(benchmark, waveform_size, backend, log_level):
 
     val = list(range(waveform_size))
     with context(pvname, initial_value=val, log_level=log_level) as bench_fcn:
+        benchmark(bench_fcn)
+
+
+@contextlib.contextmanager
+def bench_pyepics_many_connections(pv_names, *, initial_value=None,
+                                   log_level='DEBUG'):
+    import epics
+
+    pvs = []
+
+    def pyepics():
+        nonlocal pvs
+
+        pvs = [epics.PV(pvname, auto_monitor=False)
+               for pvname in pv_names]
+
+        while not all(pv.connected for pv in pvs):
+            time.sleep(0.01)
+
+    epics.ca.clear_cache()
+
+    try:
+        yield pyepics
+    finally:
+        for pv in pvs:
+            pv.disconnect()
+
+        epics.ca.clear_cache()
+
+
+@contextlib.contextmanager
+def bench_threading_many_connections(pv_names, *, initial_value=None,
+                                     log_level='DEBUG'):
+    from caproto.threading.client import (PV, SharedBroadcaster,
+                                          Context as ThreadingContext)
+
+    shared_broadcaster = SharedBroadcaster()
+    context = ThreadingContext(broadcaster=shared_broadcaster,
+                               log_level=log_level)
+
+    pvs = []
+
+    def threading():
+        nonlocal pvs
+
+        pvs = [PV(pvname, auto_monitor=False, context=context)
+               for pvname in pv_names]
+
+        while not all(pv.connected for pv in pvs):
+            time.sleep(0.01)
+
+    try:
+        yield threading
+    finally:
+        for pv in pvs:
+            pv.disconnect()
+
+        shared_broadcaster.disconnect()
+    logger.debug('Done')
+
+
+@contextlib.contextmanager
+def bench_curio_many_connections(pv_names, *, initial_value=None,
+                                 log_level='DEBUG'):
+    kernel = curio.Kernel()
+
+    async def test():
+        broadcaster = ca.curio.client.SharedBroadcaster(
+            log_level=log_level)
+        await broadcaster.register()
+        ctx = ca.curio.client.Context(broadcaster, log_level=log_level)
+
+        pvs = {}
+        async with curio.TaskGroup() as connect_task:
+            async with curio.TaskGroup() as search_task:
+                for pvname in pv_names:
+                    await search_task.spawn(ctx.search, pvname)
+
+                while True:
+                    res = await search_task.next_done()
+                    if res is None:
+                        break
+                    pvname = res.result
+                    await connect_task.spawn(ctx.create_channel, pvname)
+
+            while True:
+                res = await connect_task.next_done()
+                if res is None:
+                    break
+                curio_channel = res.result
+                pvname = curio_channel.channel.name
+                pvs[pvname] = curio_channel
+
+        assert len(pvs) == len(pv_names)
+        # TODO: can't successfully test as this hammers file creation; this
+        # will be important to resolve...
+        await curio.sleep(1)
+
+    def curio_client():
+        kernel.run(test())
+
+    yield curio_client
+
+    logger.debug('Shutting down the kernel')
+    kernel.run(shutdown=True)
+    logger.debug('Done')
+
+
+@pytest.mark.parametrize('connection_count', [5])
+@pytest.mark.parametrize('pv_format', ['connections:{}'])
+@pytest.mark.parametrize('backend', ['curio', 'pyepics', 'threading'])
+@pytest.mark.parametrize('log_level', ['INFO'])
+def test_many_connections(benchmark, backend, connection_count, pv_format,
+                          log_level):
+    ca.benchmarking.set_logging_level(logging.DEBUG, logger=logger)
+
+    context = {'pyepics': bench_pyepics_many_connections,
+               'curio': bench_curio_many_connections,
+               'threading': bench_threading_many_connections,
+               }[backend]
+
+    pv_names = [pv_format.format(i) for i in range(connection_count)]
+    with context(pv_names, log_level=log_level) as bench_fcn:
         benchmark(bench_fcn)
