3d3a2cce2d7c5369e4b66e6d27ac308dc70c7f80
K Lauer <kenneth.lauer@gmail.com>
2017-11-14 13:16:24 -0800

    ENH: add incremental buffer sending utilities

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -19,3 +19,52 @@ def test_ensure_bytes():
     assert ca.ensure_bytes(b'abc\0') == b'abc\0'
     with pytest.raises(ca.CaprotoTypeError):
         ca.ensure_bytes(1)
+
+
+_incr_sends = [
+    [(b'abc', b'def', b'ghi'),
+     0,
+     (b'abc', b'def', b'ghi')
+     ],
+
+    [(b'abc', b'def', b'ghi'),
+     1,
+     (b'bc', b'def', b'ghi')
+     ],
+
+    [(b'abc', b'def', b'ghi'),
+     3,
+     (b'def', b'ghi')
+     ],
+
+    [(ca.MessageHeader(0, 1, 2, 3, 4, 5), b'def'),
+     0,
+     (bytes(ca.MessageHeader(0, 1, 2, 3, 4, 5)), b'def'),
+     ],
+
+    [(ca.MessageHeader(0, 1, 2, 3, 4, 5), b'def'),
+     5,
+     (bytes(ca.MessageHeader(0, 1, 2, 3, 4, 5))[5:], b'def'),
+     ],
+]
+
+
+@pytest.mark.parametrize('buffers, offset, expected', _incr_sends)
+def test_buffer_list_slice(buffers, offset, expected):
+    assert ca.buffer_list_slice(*buffers, offset=offset) == expected
+
+
+@pytest.mark.parametrize('buffers, offset, expected', _incr_sends)
+def test_incremental_send(buffers, offset, expected):
+    full_bytes = b''.join(bytes(b) for b in buffers)
+
+    gen = ca.incremental_buffer_list_slice(*buffers)
+    gen.send(None)
+
+    for i in range(len(full_bytes)):
+        try:
+            buffers = gen.send(1)
+        except StopIteration:
+            assert i == (len(full_bytes) - 1), 'StopIteration unexpected'
+            break
+        assert full_bytes[i + 1:] == b''.join(bytes(b) for b in buffers)
