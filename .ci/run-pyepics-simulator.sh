86eb4849b96e2bf87b06988f72deca7aaeb2f7f5
danielballan <daniel.b.allan@gmail.com>
2017-11-14 11:03:13 -0800

    CI: Manually tell pyepics where to find libca.

--- .ci/run-pyepics-simulator.sh
+++ .ci/run-pyepics-simulator.sh
@@ -2,6 +2,7 @@
 set -e -x
 
 source $TRAVIS_BUILD_DIR/.ci/epics-config.sh
+export PYEPICS_LIBCA=$HOME/.cache/support/epics-base/lib/${EPICS_HOST_ARCH}/libca.so
 
 echo "Running pyepics simulator program..."
 python ${PYEPICS_IOC}/simulator.py &
