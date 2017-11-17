670f9450cd1fcac32be11109184574ab18d3100d
danielballan <daniel.b.allan@gmail.com>
2017-11-17 09:35:09 -0800

    DEV: Add PYEPICS_LIBCA to local dev script.

--- setup_local_dev_env.sh
+++ setup_local_dev_env.sh
@@ -1,3 +1,4 @@
+export EPICS_HOST_ARCH=linux-x86_64
 export EPICS_CA_ADDR_LIST=127.255.255.255
 export EPICS_CA_AUTO_ADDR_LIST=NO
 export EPICS_CA_MAX_ARRAY_BYTES=10000000
@@ -10,3 +11,4 @@ export CALC=R3-6-1
 export MOTOR=6-9
 # mock Travis
 export TRAVIS_BUILD_DIR=${PWD}
+export PYEPICS_LIBCA=$HOME/.cache/support/epics-base/lib/${EPICS_HOST_ARCH}/libca.so
