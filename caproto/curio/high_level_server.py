423a0800aae741d8760990899e1735b615a20ab1
K Lauer <kenneth.lauer@gmail.com>
2018-01-13 11:20:19 -0800

    MNT: move pvproperty example to examples directory

--- caproto/curio/high_level_server.py
+++ caproto/curio/high_level_server.py
@@ -5,14 +5,13 @@ from types import MethodType
 
 from .. import (ChannelDouble, ChannelInteger, ChannelString,
                 ChannelAlarm)
-from ..benchmarking import set_logging_level
-from .server import start_server
 
 
 logger = logging.getLogger(__name__)
 
 
-class PVSpec(namedtuple('PVSpec', 'get put attr name dtype value alarm_group')):
+class PVSpec(namedtuple('PVSpec',
+                        'get put attr name dtype value alarm_group')):
     'PV information specification'
     __slots__ = ()
     default_dtype = int
@@ -193,78 +192,3 @@ class PVGroupBase(metaclass=PVGroupMeta):
         'Generic write called for channels without `put` defined'
         logger.debug('group write of %s = %s', instance.pvspec.attr, value)
         return value
-
-
-class MyPVGroup(PVGroupBase):
-    'Example group of PVs, where the prefix is defined on instantiation'
-    # starting off with something like old-style property-like PVs:
-
-    # PV #1: {prefix}single
-    # - has custom read/write methods defined in the wrapped function
-    async def single_read(self, instance):
-        # NOTE: self is the group, instance is the channeldata instance
-        logger.debug('read single')
-        return ['a']
-
-    async def single_write(self, instance, value):
-        logger.debug('write single %r', value)
-        return value
-
-    single = pvproperty(single_read, single_write, value=['b'])
-
-    async def read_int(self, instance):
-        logger.debug('read_int of %s', instance.pvspec.attr)
-        return instance.value
-
-    # a set of 3 PVs re-using the same spec-func
-    # PV #2: {prefix}testa
-    testa = pvproperty(read_int, value=[1])
-    # PV #3: {prefix}testa
-    testb = pvproperty(read_int, value=[2])
-    # PV #4: {prefix}testa
-    testc = pvproperty(read_int, value=[3])
-
-    # PV #5: {prefix}{macro}
-    macroified = pvproperty(value=[0], name='{macro}')
-
-    # - section 2 - with new-style property-like PVs
-    # PV #6: {prefix}newstyle
-    @pvproperty(value=['c'])
-    async def newstyle(self, instance):
-        logger.debug('read newstyle')
-        return instance.value
-
-    @newstyle.putter
-    async def newstyle(self, instance, value):
-        logger.debug('write newstyle %r', value)
-        return value
-
-    # PV #7: {prefix}random - defaults to dtype of int
-    @pvproperty
-    async def random(self, instance):
-        logger.debug('read random')
-        import random
-        return random.randint(1, 100)
-
-
-def main():
-    import curio
-    from pprint import pprint
-
-    set_logging_level(logging.DEBUG)
-    logger.setLevel(logging.DEBUG)
-    logging.basicConfig()
-
-    logger.info('Starting up')
-    ioc = MyPVGroup(prefix='prefix:', macros=dict(macro='expanded'))
-
-    pprint(ioc.pvdb)
-
-    print('random using the descriptor getter is:', ioc.random)
-    print('single pvspec is:', ioc.single.pvspec)
-
-    curio.run(start_server, ioc.pvdb)
-
-
-if __name__ == '__main__':
-    main()
