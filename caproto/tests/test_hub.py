f31244fabce68a3e1a5453e0705820b9e0050e9f
Thomas A Caswell <tcaswell@bnl.gov>
2018-04-07 20:06:19 -0400

    TMP-TST: Do not test relaxation of protocol
    To go with changes in 7772849be50f1fe3d075d55d25632d2d55ba4026 to
    _broadcaster.Broadcaster_process_commands to not enforce that a
    version response come before a search response.

--- caproto/tests/test_hub.py
+++ caproto/tests/test_hub.py
@@ -191,9 +191,11 @@ def test_broadcaster_checks():
     res = ca.SearchResponse(port=6666, ip='1.2.3.4', cid=0, version=13)
     addr = ('1.2.3.4', 6666)
     commands = b.recv(bytes(res), addr)
-    with pytest.raises(ca.RemoteProtocolError):
-        b.process_commands(commands)
-    commands = b.recv(bytes(ca.VersionResponse(version=13)) + bytes(res), addr)
+    # see changes to _broadcaster.py.  Apparently rsrv does not always conform
+    # to the protocol and include a version response before search responsesq
+    # with pytest.raises(ca.RemoteProtocolError):
+    #     b.process_commands(commands)
+    # commands = b.recv(bytes(ca.VersionResponse(version=13)) + bytes(res), addr)
     b.process_commands(commands)  # this gets both
 
 
@@ -358,4 +360,3 @@ def test_circuit_equality():
     b = ca.VirtualCircuit(ca.CLIENT, ('asdf', 1234), 1)
     c = ca.VirtualCircuit(ca.CLIENT, ('asdf', 1234), 2)
     assert a == b != c
-
