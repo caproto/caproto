e94d5cb76c1338a428efd4df2109ed114688d5b1
Thomas A Caswell <tcaswell@bnl.gov>
2018-05-02 08:52:39 -0400

    ENH: we don't need no stinking locks

--- caproto/threading/pyepics_compat.py
+++ caproto/threading/pyepics_compat.py
@@ -173,7 +173,6 @@ class PV:
         self.auto_monitor = auto_monitor
         self.ftype = None
         self._connect_event = threading.Event()
-        self._state_lock = threading.RLock()
         self.connection_timeout = connection_timeout
         self.default_count = count
         self._auto_monitor_sub = None
@@ -235,23 +234,19 @@ class PV:
         if timeout is None:
             timeout = self.connection_timeout
 
-        with self._state_lock:
-            if self.connected:
-                return True
-            self._connect_event.clear()
+        if self.connected:
+            return True
 
         self._caproto_pv.wait_for_connection(timeout=timeout)
-        with self._state_lock:
-            # TODO shorten timeouts based on the above
-            ok = self._connect_event.wait(timeout=timeout)
-            ok = ok and self.connected
+        # TODO shorten timeouts based on the above
+        ok = self.connected
 
-            if not ok:
-                raise TimeoutError(f'{self.pvname} failed to connect within '
-                                   f'{timeout} seconds '
-                                   f'(caproto={self._caproto_pv})')
+        if not ok:
+            raise TimeoutError(f'{self.pvname} failed to connect within '
+                               f'{timeout} seconds '
+                               f'(caproto={self._caproto_pv})')
 
-            return True
+        return True
 
     def _connection_established(self):
         'Callback when connection is initially established'
@@ -259,29 +254,28 @@ class PV:
         ch = self._caproto_pv.channel
         form = self.form
         count = self.default_count
-        with self._state_lock:
-            if ch is None:
-                logger.error('Connection dropped in connection callback')
-                return
+        if ch is None:
+            logger.error('Connection dropped in connection callback')
+            return
 
-            self._args['type'] = ch.native_data_type
+        self._args['type'] = ch.native_data_type
 
-            type_key = 'control' if form == 'ctrl' else form
-            self._args['typefull'] = field_types[type_key][ch.native_data_type]
-            self._args['nelm'] = ch.native_data_count
-            self._args['count'] = ch.native_data_count
+        type_key = 'control' if form == 'ctrl' else form
+        self._args['typefull'] = field_types[type_key][ch.native_data_type]
+        self._args['nelm'] = ch.native_data_count
+        self._args['count'] = ch.native_data_count
 
-            self._args['write_access'] = AccessRights.WRITE in ch.access_rights
-            self._args['read_access'] = AccessRights.READ in ch.access_rights
+        self._args['write_access'] = AccessRights.WRITE in ch.access_rights
+        self._args['read_access'] = AccessRights.READ in ch.access_rights
 
-            access_strs = ('no access', 'read-only', 'write-only', 'read/write')
-            self._args['access'] = access_strs[ch.access_rights]
+        access_strs = ('no access', 'read-only', 'write-only', 'read/write')
+        self._args['access'] = access_strs[ch.access_rights]
 
-            if self.auto_monitor is None:
-                mcount = count if count is not None else self._args['count']
-                self.auto_monitor = mcount < AUTOMONITOR_MAXLENGTH
+        if self.auto_monitor is None:
+            mcount = count if count is not None else self._args['count']
+            self.auto_monitor = mcount < AUTOMONITOR_MAXLENGTH
 
-            self._check_auto_monitor_sub()
+        self._check_auto_monitor_sub()
 
     def _check_auto_monitor_sub(self, count=None):
         'Ensure auto-monitor subscription is running'
@@ -297,15 +291,12 @@ class PV:
     def _connection_state_changed(self, caproto_pv, state):
         'Connection callback hook from threading.PV.connection_state_changed'
         connected = (state == 'connected')
-        with self._state_lock:
-            try:
-                if connected:
-                    self._connection_established()
-            except Exception as ex:
-                logger.exception('Connection state callback failed!')
-                raise
-            finally:
-                self._connect_event.set()
+        try:
+            if connected:
+                self._connection_established()
+        except Exception as ex:
+            logger.exception('Connection state callback failed!')
+            raise
 
         # todo move to async connect logic
         for cb in self.connection_callbacks:
