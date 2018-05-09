4bcdcbd2687f2997b6ad880e9cf80d766c53e4c4
Thomas A Caswell <tcaswell@bnl.gov>
2018-05-08 20:45:07 -0400

    BLD: add classifiers
    closes #181

--- setup.py
+++ setup.py
@@ -4,6 +4,16 @@ import setuptools  # noqa F401
 import versioneer
 
 
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'License :: OSI Approved :: BSD License'
+]
+
 setup(name='caproto',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
@@ -23,4 +33,5 @@ setup(name='caproto',
                 ],
       scripts=glob.glob('scripts/*'),
       python_requires='>=3.6',
+      classifiers=classifiers
       )
