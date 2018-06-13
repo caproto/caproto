f6a3cd4f330c8c94c698ac584891b6793be3465d
danielballan <daniel.b.allan@gmail.com>
2018-06-13 12:03:37 -0500

    TST: Test array and ts filters.

---
+++ caproto/tests/test_channel_filters.py
@@ -0,0 +1,102 @@
+import pytest
+import threading
+import time
+from ..threading.client import Context
+from .conftest import run_example_ioc
+
+
+@pytest.fixture(scope='function')
+def context(request):
+    context = Context()
+
+    def cleanup():
+        sb = context.broadcaster
+        context.disconnect()
+        assert not context._process_search_results_thread.is_alive()
+        assert not context._activate_subscriptions_thread.is_alive()
+        assert not context.selector.thread.is_alive()
+        sb.disconnect()
+        assert not sb._command_thread.is_alive()
+        assert not sb.selector.thread.is_alive()
+        assert not sb._retry_unanswered_searches_thread.is_alive()
+
+    request.addfinalizer(cleanup)
+    return context
+
+
+@pytest.mark.parametrize('filter, expected',
+                         [('[4]', [5]),
+                          ('[:3]', [1, 1, 2, 3]),
+                          ('[2:3]', [2, 3]),
+                          ('[2:2:6]', [2, 5, 13]),
+                          ('{"arr": {"s": 3, "e": 3}}', [3]),
+                          ('{"arr": {"s": 0, "e": 3}}', [1, 1, 2, 3]),
+                          ('{"arr": {"s": 2, "e": 3}}', [2, 3]),
+                          ('{"arr": {"s": 2, "e": 6, "i": 2}}', [2, 5, 13])
+                          ])
+def test_array_filter(request, prefix, context, filter, expected):
+    pv_name = f'{prefix}fib'
+    run_example_ioc('caproto.ioc_examples.type_varieties', request=request,
+                    args=['--prefix', prefix],
+                    pv_to_check=pv_name)
+
+    pv, = context.get_pvs(pv_name + '.' + filter)
+    pv.wait_for_connection()
+    event = threading.Event()
+
+    def cb(reading):
+        assert list(reading.data) == expected
+        event.set()
+    # Test read.
+    reading = pv.read()
+    cb(reading)
+    # Test subscribe.
+    sub = pv.subscribe()
+    event.clear()
+    sub.add_callback(cb)
+    # Wait for callback to process.
+    event.wait(timeout=2)
+
+
+def test_ts_filter(request, prefix, context):
+    pv_name = f'{prefix}fib'
+    run_example_ioc('caproto.ioc_examples.type_varieties', request=request,
+                    args=['--prefix', prefix],
+                    pv_to_check=pv_name)
+
+    # Access one element.
+    pv, = context.get_pvs(pv_name)
+    ts_pv, = context.get_pvs(pv_name + '.{"ts": {}}')
+    pv.wait_for_connection()
+    ts_pv.wait_for_connection()
+
+    # Check that proc_time is stable across subsequent reads.
+    proc_time = pv.read(data_type='time').metadata.timestamp
+    time.sleep(0.05)
+    assert proc_time == pv.read(data_type='time').metadata.timestamp
+
+    event = threading.Event()
+
+    def cb(reading):
+        assert reading.metadata.timestamp != proc_time
+        event.set()
+    # Test read.
+    reading = ts_pv.read(data_type='time')
+    cb(reading)
+    # Test subscribe.
+    sub = pv.subscribe(data_type='time')
+    event.clear()
+    sub.add_callback(cb)
+    # Wait for callback to process.
+    event.wait(timeout=2)
+
+
+def test_sync(request, prefix):
+    pv = f'{prefix}toggle_state'
+    run_example_ioc('caproto.ioc_examples.states', request=request,
+                    args=['--prefix', prefix],
+                    pv_to_check=pv)
+    responses = []
+
+    def cache(response):
+        responses.append(response)
