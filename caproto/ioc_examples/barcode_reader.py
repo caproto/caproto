279413fd4053ab055a720a0334e385f1b470f072
Thomas A Caswell <tcaswell@bnl.gov>
2018-06-14 12:05:02 -0500

    MNT: refactor barcode reader a bit

--- caproto/ioc_examples/barcode_reader.py
+++ caproto/ioc_examples/barcode_reader.py
@@ -1,38 +1,48 @@
 #!/usr/bin/env python3
 """
 This example requires a barcode reader and the Python library evdev.
+
+This only works on linux (as it uses the linux kernel input events).  You
+must edit this file to point at the correct file descriptor in /dev/input
+for your barcode reader.
 """
 from caproto.server import pvproperty, PVGroup, ioc_arg_parser, run
 from evdev import InputDevice, categorize
 
 
 class BarcodeIOC(PVGroup):
-    def __init__(self, *args, event_path, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, event_path, **kwargs):
+        super().__init__(**kwargs)
         self._event_path = event_path
 
     barcode = pvproperty(value=[''])
 
     @barcode.startup
     async def barcode(self, instance, async_lib):
-        local = []
-        dev = InputDevice(self._event_path)
-        with dev.grab_context():
-            async for ev in dev.async_read_loop():
-                if ev.type != 1:
-                    continue
-                ev = categorize(ev)
-                if ev.keycode == 'KEY_ENTER':
-                    read = ''.join([ev.keycode.split('_')[1]
-                                    for ev in local
-                                    if ev.keystate])
-                    if not read:
-                        continue
-                    await instance.write(value=read)
-                    local.clear()
+        async for read in barcode_driver(InputDevice(self._event_path)):
+            await instance.write(value=read)
 
-                else:
-                    local.append(ev)
+
+async def barcode_driver(dev):
+    local = []
+    with dev.grab_context():
+        async for ev in dev.async_read_loop():
+            if ev.type != 1:
+                continue
+            ev = categorize(ev)
+            if ev.keycode == 'KEY_ENTER':
+                read = ''.join([
+                    ev.keycode.split('_')[1]
+                    for ev in local
+                    if (ev.keystate and 'SHIFT' not in ev.keycode)
+                ])
+                if not read:
+                    continue
+                yield read
+                local.clear()
+
+            else:
+                local.append(ev)
 
 
 if __name__ == '__main__':
@@ -40,5 +50,6 @@ if __name__ == '__main__':
         default_prefix='bc:',
         desc='Run an IOC that updates when a barcode is read.',
         supported_async_libs=('asyncio',))
+    # Edit this line to point at the correct event
     ioc = BarcodeIOC(event_path='/dev/input/event17', **ioc_options)
     run(ioc.pvdb, **run_options)
