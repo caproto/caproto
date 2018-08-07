caproto/caproto

commit 7f7f2e43ba4cd6d3487628d6ec9ba1d36bc9adff

API: Add `pv` to access_rights_callback signature.

caproto/threading/pyepics_compat.py
@@ -501,7 +501,8 @@ class PV:
         self._access_rights_changed(self._caproto_pv.channel.access_rights,
                                     forced=True)
 
-    def _access_rights_changed(self, access_rights, *, forced=False):
+    def _access_rights_changed(self, caproto_pv, access_rights, *,
+                               forced=False):
         read_access = AccessRights.READ in access_rights
         write_access = AccessRights.WRITE in access_rights
         access_strs = ('no access', 'read-only', 'write-only', 'read/write')

caproto/threading/client.py
@@ -739,7 +739,8 @@ class Context:
             Expected signature: ``f(pv, state)`` where ``pv`` is the instance
             of ``PV`` whose state has changed and ``state`` is a string
         access_rights_callback : callable
-            Expected signature: ``f(access_rights)`` where ``access_rights`` is
+            Expected signature: ``f(pv, access_rights)`` where ``pv`` is the
+            instance of ``PV`` whose state has changed and ``access_rights`` is
             a member of the caproto ``AccessRights`` enum
 
         """
@@ -1291,7 +1292,7 @@ class PV:
             self._channel = val
 
     def access_rights_changed(self, rights):
-        self.access_rights_callback.process(rights)
+        self.access_rights_callback.process(self, rights)
 
     def connection_state_changed(self, state, channel):
         self.log.info('%s connection state changed to %s.', self.name, state)

commit f468fe9854f37cda9922dc010f52eb3951e0222c

FIX: Update internal calls to new sig of _accces_rights_changed.
Bug was introduced in 7f7f2e43.

caproto/threading/pyepics_compat.py
@@ -287,7 +287,7 @@ class PV:
             nelm=ch.native_data_count,
             count=ch.native_data_count,
         )
-        self._access_rights_changed(ch.access_rights)
+        self._access_rights_changed(caproto_pv, ch.access_rights)
 
         if self.auto_monitor is None:
             mcount = count if count is not None else ch.native_data_count
@@ -498,7 +498,8 @@ class PV:
     @ensure_connection
     def force_read_access_rights(self):
         'Force a read of access rights, not relying on last event callback.'
-        self._access_rights_changed(self._caproto_pv.channel.access_rights,
+        self._access_rights_changed(self._caproto_pv,
+                                    self._caproto_pv.channel.access_rights,
                                     forced=True)
 
     def _access_rights_changed(self, caproto_pv, access_rights, *,