caproto/caproto

commit 349757ae9dd2628a27ccfb5f561caf4fe3f263c6

MNT: Tweak server logging.
- Show high-load related messages in -v mode.
- Accept -vvvvv as a synonym for -vvv, same as CLI clients.
- Make description in -h consistent across CLIs.

And make descritpion consistent across CLIs.

caproto/server/server.py
@@ -1111,10 +1111,8 @@ def template_arg_parser(*, desc, default_prefix, argv=None, macros=None,
     group.add_argument('-q', '--quiet', action='store_true',
                        help=("Suppress INFO log messages. "
                              "(Still show WARNING or higher.)"))
-    group.add_argument('-v', '--verbose', action='store_true',
-                       help="Verbose mode. (Use -vvv for more.)")
-    group.add_argument('-vvv', action='store_true',
-                       help=argparse.SUPPRESS)
+    group.add_argument('-v', '--verbose', action='count',
+                        help="Show more log messages. (Use -vvv for even more.)")
     parser.add_argument('--list-pvs', action='store_true',
                         help="At startup, log the list of PV names served.")
     choices = tuple(supported_async_libs or ('asyncio', 'curio', 'trio'))
@@ -1154,16 +1152,16 @@ def template_arg_parser(*, desc, default_prefix, argv=None, macros=None,
         run_options : dict
             kwargs to be handed to run
         """
-        if args.vvv:
+        if args.verbose > 1:
             logging.getLogger('caproto').setLevel('DEBUG')
         else:
             if args.verbose:
-                level = 'DEBUG'
+                logging.getLogger('caproto.ctx').setLevel('DEBUG')
+                logging.getLogger('caproto.circ').setLevel('INFO')
             elif args.quiet:
-                level = 'WARNING'
+                logging.getLogger('caproto').setLevel('WARNING')
             else:
-                level = 'INFO'
-            logging.getLogger('caproto.ctx').setLevel(level)
+                logging.getLogger('caproto.ctx').setLevel('INFO')
 
         return ({'prefix': args.prefix,
                  'macros': {key: getattr(args, key) for key in macros}},

caproto/commandline/get.py
@@ -24,7 +24,7 @@ def main():
     parser.add_argument('pv_names', type=str, nargs='+',
                         help="PV (channel) name(s) separated by spaces")
     parser.add_argument('--verbose', '-v', action='count',
-                        help="Verbose mode. (Use -vvv for more.)")
+                        help="Show more log messages. (Use -vvv for even more.)")
     fmt_group.add_argument('--format', type=str,
                            help=("Python format string. Available tokens are "
                                  "{pv_name} and {response}. Additionally, if "

caproto/commandline/monitor.py
@@ -33,7 +33,7 @@ def main():
                                  "like {timestamp:%%Y-%%m-%%d %%H:%%M:%%S} are"
                                  " supported."))
     parser.add_argument('--verbose', '-v', action='count',
-                        help="Show DEBUG log messages.")
+                        help="Show more log messages. (Use -vvv for even more.)")
     exit_group.add_argument('--duration', type=float, default=None,
                             help=("Maximum number seconds to run before "
                                   "exiting. Runs indefinitely by default."))

caproto/commandline/put.py
@@ -26,7 +26,7 @@ def main():
     parser.add_argument('data', type=str,
                         help="Value or values to write.")
     parser.add_argument('--verbose', '-v', action='count',
-                        help="Show DEBUG log messages.")
+                        help="Show more log messages. (Use -vvv for even more.)")
     fmt_group.add_argument('--format', type=str,
                            help=("Python format string. Available tokens are "
                                  "{pv_name}, {response} and {which} (Old/New)."

commit 66258571a0c59a2e2fbdb908dee03f2218019764

FIX: Arg-parsing logic was broken by 349757a.

caproto/server/server.py
@@ -1152,16 +1152,16 @@ def template_arg_parser(*, desc, default_prefix, argv=None, macros=None,
         run_options : dict
             kwargs to be handed to run
         """
-        if args.verbose > 1:
-            logging.getLogger('caproto').setLevel('DEBUG')
-        else:
-            if args.verbose:
+        if args.verbose:
+            if args.verbose > 1:
+                logging.getLogger('caproto').setLevel('DEBUG')
+            else:
                 logging.getLogger('caproto.ctx').setLevel('DEBUG')
                 logging.getLogger('caproto.circ').setLevel('INFO')
-            elif args.quiet:
-                logging.getLogger('caproto').setLevel('WARNING')
-            else:
-                logging.getLogger('caproto.ctx').setLevel('INFO')
+        elif args.quiet:
+            logging.getLogger('caproto').setLevel('WARNING')
+        else:
+            logging.getLogger('caproto.ctx').setLevel('INFO')
 
         return ({'prefix': args.prefix,
                  'macros': {key: getattr(args, key) for key in macros}},