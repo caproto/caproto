88ff6d39547a6c5bb5ff13d0d7fb0f832b45691a
Thomas A Caswell <tcaswell@bnl.gov>
2018-10-16 11:30:00 -0400

    STY: appease flake8

--- caproto/tests/test_examples.py
+++ caproto/tests/test_examples.py
@@ -342,6 +342,7 @@ def _test_ioc_examples(request, module_name, pvdb_class_name, class_kwargs,
         value = read(pv, timeout=15)
         print(f'Read {pv} = {value}')
 
+
 # TODO sort out why these are flaky on travis
 @pytest.mark.flaky(reruns=2, reruns_delay=2)
 @pytest.mark.parametrize(
