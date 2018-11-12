de982424a8f0c52fe3a434edd9d615a32289f0c0
Thomas A Caswell <tcaswell@bnl.gov>
2018-11-12 22:37:44 +1100

    ENH: use _Limits base class for Calcout

--- caproto/server/records.py
+++ caproto/server/records.py
@@ -1454,7 +1454,7 @@ class CalcFields(RecordFieldGroup, _Limits):
 
 
 @register_record
-class CalcoutFields(RecordFieldGroup):
+class CalcoutFields(RecordFieldGroup, _Limits):
     _record_type = 'calcout'
     # value = pvproperty(name='VAL', dtype=ChannelType.DOUBLE, doc='Result')
     alarm_status = pvproperty(
@@ -1501,34 +1501,6 @@ class CalcoutFields(RecordFieldGroup):
         name='PVAL', dtype=ChannelType.DOUBLE, doc='Previous Value')
     alarm_deadband = pvproperty(
         name='HYST', dtype=ChannelType.DOUBLE, doc='Alarm Deadband')
-    high_alarm_limit = pvproperty(
-        name='HIGH', dtype=ChannelType.DOUBLE, doc='High Alarm Limit')
-    high_severity = pvproperty(
-        name='HSV',
-        dtype=ChannelType.ENUM,
-        enum_strings=menus.menuAlarmSevr.get_string_tuple(),
-        doc='High Severity')
-    hihi_alarm_limit = pvproperty(
-        name='HIHI', dtype=ChannelType.DOUBLE, doc='Hihi Alarm Limit')
-    hihi_severity = pvproperty(
-        name='HHSV',
-        dtype=ChannelType.ENUM,
-        enum_strings=menus.menuAlarmSevr.get_string_tuple(),
-        doc='Hihi Severity')
-    lolo_alarm_limit = pvproperty(
-        name='LOLO', dtype=ChannelType.DOUBLE, doc='Lolo Alarm Limit')
-    lolo_severity = pvproperty(
-        name='LLSV',
-        dtype=ChannelType.ENUM,
-        enum_strings=menus.menuAlarmSevr.get_string_tuple(),
-        doc='Lolo Severity')
-    low_alarm_limit = pvproperty(
-        name='LOW', dtype=ChannelType.DOUBLE, doc='Low Alarm Limit')
-    low_severity = pvproperty(
-        name='LSV',
-        dtype=ChannelType.ENUM,
-        enum_strings=menus.menuAlarmSevr.get_string_tuple(),
-        doc='Low Severity')
     output_execute_delay = pvproperty(
         name='ODLY', dtype=ChannelType.DOUBLE, doc='Output Execute Delay')
     calculation = pvproperty(
@@ -1554,10 +1526,6 @@ class CalcoutFields(RecordFieldGroup):
         name='ADEL', dtype=ChannelType.DOUBLE, doc='Archive Deadband')
     display_precision = pvproperty(
         name='PREC', dtype=ChannelType.LONG, doc='Display Precision')
-    high_operating_rng = pvproperty(
-        name='HOPR', dtype=ChannelType.DOUBLE, doc='High Operating Rng')
-    low_operating_range = pvproperty(
-        name='LOPR', dtype=ChannelType.DOUBLE, doc='Low Operating Range')
     monitor_deadband = pvproperty(
         name='MDEL', dtype=ChannelType.DOUBLE, doc='Monitor Deadband')
     units_name = pvproperty(
@@ -1572,11 +1540,6 @@ class CalcoutFields(RecordFieldGroup):
     output_specification = pvproperty(
         name='OUT', dtype=ChannelType.STRING, doc='Output Specification')
     _link_parent_attribute(display_precision, 'precision')
-    _link_parent_attribute(hihi_alarm_limit, 'upper_alarm_limit')
-    _link_parent_attribute(high_alarm_limit, 'upper_warning_limit')
-    _link_parent_attribute(low_alarm_limit, 'lower_warning_limit')
-    _link_parent_attribute(lolo_alarm_limit, 'lower_alarm_limit')
-    _link_parent_attribute(low_operating_range, 'lower_ctrl_limit')
     _link_parent_attribute(archive_deadband, 'log_atol')
     _link_parent_attribute(monitor_deadband, 'value_atol')
 
