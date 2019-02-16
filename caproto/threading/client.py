2424502a0a030474352fe353c15405c0bd20cf10
K Lauer <kenneth.lauer@gmail.com>
2019-02-16 09:11:07 -0800

    FIX/REF: do not attempt reconnection when user requests disconnect

--- caproto/threading/client.py
+++ caproto/threading/client.py
@@ -1152,7 +1152,7 @@ class Context:
                 if circuit.connected:
                     self.log.debug('Disconnecting circuit %d/%d: %s',
                                    idx, total_circuits, circuit)
-                    circuit.disconnect()
+                    circuit._disconnected(reconnect=False)
                     disconnected = True
             if disconnected:
                 self.log.debug('All circuits disconnected')
@@ -1392,7 +1392,7 @@ class VirtualCircuitManager:
         else:
             self.log.debug('other command %s', command)
 
-    def _disconnected(self):
+    def _disconnected(self, *, reconnect=True):
         # Ensure that this method is idempotent.
         if self.dead.is_set():
             return
@@ -1432,12 +1432,15 @@ class VirtualCircuitManager:
 
             sock.close()
 
-        # Kick off attempt to reconnect all PVs via fresh circuit(s).
-        self.log.debug('Kicking off reconnection attempts for %d PVs '
-                       'disconnected from %s:%d....',
-                       len(self.channels), *self.circuit.address)
-        self.context.reconnect(((chan.name, chan.circuit.priority)
-                                for chan in self.channels.values()))
+        if reconnect:
+            # Kick off attempt to reconnect all PVs via fresh circuit(s).
+            self.log.debug('Kicking off reconnection attempts for %d PVs '
+                           'disconnected from %s:%d....',
+                           len(self.channels), *self.circuit.address)
+            self.context.reconnect(((chan.name, chan.circuit.priority)
+                                    for chan in self.channels.values()))
+        else:
+            self.log.debug('Not attempting reconnection')
 
         self.log.debug("Shutting down ThreadPoolExecutor for user callbacks")
         self.user_callback_executor.shutdown()
@@ -1447,19 +1450,11 @@ class VirtualCircuitManager:
         if self.socket is None:
             return
 
-        sock, self.socket = self.socket, None
-        try:
-            sock.shutdown(socket.SHUT_WR)
-        except OSError:
-            pass
-
-        sock.close()
-
         self.log.debug('Circuit manager disconnected by user')
 
     def __del__(self):
         try:
-            self.disconnect()
+            self._disconnected(reconnect=False)
         except AttributeError:
             pass
 
