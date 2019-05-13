caproto/caproto

commit 6ca54b76d7ba09abaa94487460628048c4e5da82

PERF: Do string concatenation in one move.

caproto/_log.py
@@ -103,18 +103,18 @@ class LogFormatter(logging.Formatter):
             self._normal = ''
 
     def format(self, record):
-        record.message = record.getMessage()
+        message = [record.getMessage()]
         if hasattr(record, 'their_address'):
-            record.message = '[%s] %s' % (':'.join(record.their_address), record.message)
+            message.append('[%s]' % ':'.join(map(str, record.their_address)))
         if hasattr(record, 'direction'):
-            record.message = '%s %s' % (record.direction, record.message)
+            message.append('%s' % record.direction)
         if hasattr(record, 'our_address'):
-            record.message = '[%s] %s' % (':'.join(record.our_address), record.message)
+            message.append('[%s]' % ':'.join(map(str, record.our_address)))
         if hasattr(record, 'pv'):
-            record.message = '[%s] %s' % (record.pv, record.message)
+            message.append('[%s]' % record.pv)
         if hasattr(record, 'role'):
-            record.message = '[%s] %s' % (record.role, record.message)
-
+            message.append('[%s]' % record.role)
+        record.message = ' '.join(message)
         record.asctime = self.formatTime(record, self.datefmt)
 
         try:

commit 3f16ed6cd651e7625d776d90ada66ae5f9cfb43d

FIX: Put message last, fixing regression from 6ca54b7

caproto/_log.py
@@ -103,7 +103,7 @@ class LogFormatter(logging.Formatter):
             self._normal = ''
 
     def format(self, record):
-        message = [record.getMessage()]
+        message = []
         if hasattr(record, 'their_address'):
             message.append('[%s]' % ':'.join(map(str, record.their_address)))
         if hasattr(record, 'direction'):
@@ -114,6 +114,7 @@ class LogFormatter(logging.Formatter):
             message.append('[%s]' % record.pv)
         if hasattr(record, 'role'):
             message.append('[%s]' % record.role)
+        message.append(record.getMessage())
         record.message = ' '.join(message)
         record.asctime = self.formatTime(record, self.datefmt)
 