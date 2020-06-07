efdb95acef09067f6a8af38467e2aae698d9c109
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2020-06-06 22:35:32 -0400

    FIX: deprecation of Event.clear()

--- caproto/trio/server.py
+++ caproto/trio/server.py
@@ -16,15 +16,43 @@ class ServerExit(Exception):
     ...
 
 
-class Event(trio.Event):
+class Event:
+    """
+    The class wraps `trio.Event` and implements `Event.clear()`
+    method that is used in the code, but deprecated in `trio`.
+    The event is cleared by creating the new event that if the
+    event is set. If the event is not set, then old event is left.
+    The class is not intended to be thread safe.
+    """
+    def __init__(self):
+        self._create_event()
+
+    def _create_event(self):
+        self._event = trio.Event()
+
+    def set(self):
+        self._event.set()
+
+    def clear(self):
+        """
+        Clearing of the event is implemented by creating a new
+        event if the event is set. It is assumed that if the event
+        is set, then it is not waited on.
+        """
+        if self._event.is_set():
+            self._create_event()
+
+    def is_set(self):
+        return self._event.is_set()
+
     async def wait(self, timeout=None):
         if timeout is not None:
             with trio.move_on_after(timeout):
-                await super().wait()
+                await self._event.wait()
                 return True
             return False
         else:
-            await super().wait()
+            await self._event.wait()
             return True
 
 
