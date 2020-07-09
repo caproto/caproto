2648a5ca999ceab944df4abd92a4e6d89d8d5987
K Lauer <klauer@slac.stanford.edu>
2020-07-09 11:51:03 -0700

    REF: clean asyncio server task handling and use shared classes

--- caproto/asyncio/utils.py
+++ caproto/asyncio/utils.py
@@ -1,6 +1,7 @@
 import asyncio
 import functools
 import inspect
+import threading
 
 import caproto as ca
 
@@ -101,25 +102,38 @@ class _TransportWrapper:
 class _TaskHandler:
     def __init__(self):
         self.tasks = []
+        self._lock = threading.Lock()
 
     def create(self, coro):
         """Schedule the execution of a coroutine object in a spawn task."""
         task = asyncio.create_task(coro)
-        self.tasks.append(task)
+        with self._lock:
+            self.tasks.append(task)
+        task.add_done_callback(self._remove_completed_task)
         return task
 
+    def _remove_completed_task(self, task):
+        try:
+            with self._lock:
+                self.tasks.remove(task)
+        except ValueError:
+            # May have been cancelled or removed otherwise
+            ...
+
     async def cancel(self, task):
-        self.tasks.remove(task)
-        await asyncio.wait(task)
+        task.cancel()
+        await task
 
     async def cancel_all(self, wait=False):
-        for task in list(self.tasks):
+        with self._lock:
+            tasks = list(self.tasks)
+            self.tasks.clear()
+
+        for task in list(tasks):
             task.cancel()
 
-        if wait:
-            await asyncio.wait(self.tasks)
-
-        self.tasks.clear()
+        if wait and len(tasks):
+            await asyncio.wait(tasks)
 
 
 class _CallbackExecutor:
