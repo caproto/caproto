4c6b865411e0a3215451862bbf1d489dfbc9f9db
K Lauer <klauer@slac.stanford.edu>
2020-07-30 14:11:23 -0700

    MNT: subgroup configurable defaults; simple stop handling

--- caproto/ioc_examples/fake_motor_record.py
+++ caproto/ioc_examples/fake_motor_record.py
@@ -14,18 +14,36 @@ def broadcast_precision_to_fields(record):
 
 
 class FakeMotor(PVGroup):
-    tick_rate_hz = 10
     motor = pvproperty(value=0.0, name='', record='motor',
                        precision=3)
+
+    def __init__(self, *args,
+                 velocity=0.1,
+                 precision=3,
+                 acceleration=1.0,
+                 resolution=1e-6,
+                 tick_rate_hz=10.,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tick_rate_hz = tick_rate_hz
+        self.defaults = {
+            'velocity': velocity,
+            'precision': precision,
+            'acceleration': acceleration,
+            'resolution': resolution,
+        }
 
     @motor.startup
     async def motor(self, instance, async_lib):
         self.async_lib = async_lib
 
+        await self.motor.write_metadata(precision=self.defaults['precision'])
         broadcast_precision_to_fields(self.motor)
 
         fields = self.motor.field_inst  # type: MotorFields
-        await fields.velocity.write(0.1)
+        await fields.velocity.write(self.defaults['velocity'])
+        await fields.seconds_to_velocity.write(self.defaults['acceleration'])
+        await fields.motor_step_size.write(self.defaults['resolution'])
 
         while True:
             dwell = 1. / self.tick_rate_hz
@@ -47,12 +65,27 @@ class FakeMotor(PVGroup):
 
             readback = fields.user_readback_value.value
             step_size = diff / num_steps if num_steps > 0 else 0.0
-            for j in range(num_steps):
-                readback += step_size
-                await fields.user_readback_value.write(readback)
-                await async_lib.library.sleep(dwell)
+            resolution = max((fields.motor_step_size.value, 1e-10))
+            for _ in range(num_steps):
+                if fields.stop.value != 0:
+                    await fields.stop.write(0)
+                    await self.motor.write(readback)
+                    break
+                if fields.stop_pause_move_go.value == 'Stop':
+                    await self.motor.write(readback)
+                    break
 
-            await fields.user_readback_value.write(target_pos)
+                readback += step_size
+                raw_readback = readback / resolution
+                await fields.user_readback_value.write(readback)
+                await fields.dial_readback_value.write(readback)
+                await fields.raw_readback_value.write(raw_readback)
+                await async_lib.library.sleep(dwell)
+            else:
+                # Only executed if we didn't break
+                await fields.user_readback_value.write(target_pos)
+
+            await fields.motor_is_moving.write(0)
             await fields.done_moving_to_value.write(1)
 
 
@@ -67,9 +100,9 @@ class FakeMotorIOC(PVGroup):
     mtr3 (motor)
     """
 
-    motor1 = SubGroup(FakeMotor, prefix='mtr1')
-    motor2 = SubGroup(FakeMotor, prefix='mtr2')
-    motor3 = SubGroup(FakeMotor, prefix='mtr3')
+    motor1 = SubGroup(FakeMotor, velocity=1., precision=3, prefix='mtr1')
+    motor2 = SubGroup(FakeMotor, velocity=2., precision=2, prefix='mtr2')
+    motor3 = SubGroup(FakeMotor, velocity=3., precision=2, prefix='mtr3')
 
 
 if __name__ == '__main__':
