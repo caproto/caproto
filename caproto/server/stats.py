11b7589abad44a4957d956bb8004bf4983ed1702
K Lauer <klauer@slac.stanford.edu>
2020-09-23 13:31:31 -0700

    WIP: rearrange and document

--- caproto/server/stats.py
+++ caproto/server/stats.py
@@ -6,24 +6,138 @@ import platform
 import sys
 import tracemalloc
 import typing
+from typing import List, Optional
 
 from .. import ChannelType, __version__
-from . import PVGroup, pvproperty
+from . import PVGroup, SubGroup, pvproperty
 from .autosave import autosaved
 
 MODULE_LOGGER = logging.getLogger(__name__)
 
 
-class StatusHelper(PVGroup):
+if typing.TYPE_CHECKING:
+    import psutil  # noqa
+
+
+def _find_top_level_pvgroup(group: PVGroup) -> PVGroup:
+    ancestor = group
+    while ancestor.parent is not None:
+        ancestor = ancestor.parent
+    return ancestor
+
+
+class BasicStatusHelper(PVGroup):
+    process_id = pvproperty(
+        value=0,
+        name='PROCESS_ID',
+        record='ai',
+        read_only=True,
+    )
+
+    parent_pid = pvproperty(
+        name='PARENT_ID',
+        record='ai',
+        doc="Parent Process ID",
+        read_only=True,
+    )
+
+    cpu_count = pvproperty(
+        value=0,
+        name='CPU_CNT',
+        record='longin',
+        read_only=True,
+        doc='Number of CPUs',
+    )
+
+    kernel_version = pvproperty(
+        value='',
+        name='KERNEL_VERS',
+        record='stringin',
+        doc='OS/Kernel Version',
+        read_only=True,
+    )
+
+    version = pvproperty(
+        value=f'{__version__} on {sys.version}',
+        name='EPICS_VERSION',
+        record='stringin',
+        doc='EPICS (caproto) version',
+        read_only=True,
+    )
+
+    engineer = pvproperty(
+        value='',
+        name='ENGINEER',
+        record='stringin',
+        read_only=True,
+    )
+
+    location = pvproperty(
+        value='',
+        name='LOCATION',
+        record='stringin',
+        read_only=True,
+    )
+
+    hostname = pvproperty(
+        value='',
+        name='HOSTNAME',
+        record='stringin',
+        read_only=True,
+    )
+
+    app_dir = pvproperty(
+        value='',
+        name='APP_DIR',
+        record='waveform',
+        max_length=255,
+        read_only=True,
+        doc='Script directory',
+    )
+
+    sysreset = pvproperty(
+        name='SYSRESET',
+        record='sub',
+        doc='IOC exit / restart (if using procServ)',
+    )
+
+    @sysreset.putter
+    async def sysreset(self, instance, value):
+        if value == 1:
+            self.log.warning('Exit requested using StatusHelper!')
+            sys.exit(0)
+
+    @process_id.startup
+    async def process_id(self, instance, async_lib):
+        await self.process_id.write(os.getpid())
+        await self.parent_pid.write(os.getppid())
+        await self.location.write(os.environ.get('LOCATION', ''))
+        await self.engineer.write(os.environ.get('ENGINEER', ''))
+        await self.kernel_version.write(platform.uname().version)
+
+
+class PeriodicStatusHelper(PVGroup):
+    """
+    An IocStats-like tool for caproto IOCs.  Includes values which update
+    on a periodic basis (:attr:`update_period`, PV ``UPD_TIME``).
+    """
+
+    _process: Optional['psutil.Process']
+    _root_pvgroup: PVGroup
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._root_pvgroup = self._find_top_level_pvgroup()
+        self._root_pvgroup = _find_top_level_pvgroup(self)
 
-    def _find_top_level_pvgroup(self):
-        ancestor = self.parent
-        while ancestor.parent is not None:
-            ancestor = ancestor.parent
-        return ancestor
+        try:
+            import psutil
+        except ImportError:
+            self.log.warning(
+                "The Python library psutil is not installed, so MEM_USED will "
+                "not be reported by StatsHelper.")
+            self._process = None
+        else:
+            self._process = psutil.Process(os.getpid())
 
     access = pvproperty(
         name='ACCESS',
@@ -47,7 +161,7 @@ class StatusHelper(PVGroup):
     async def start_tod(self, instance, async_lib):
         await self.start_tod.write(value=str(datetime.datetime.now()))
 
-    tod = pvproperty(
+    time_of_day = pvproperty(
         value='',
         name='TOD',
         max_length=100,
@@ -56,9 +170,10 @@ class StatusHelper(PVGroup):
         read_only=True
     )
 
-    @tod.scan(period=1)
-    async def tod(self, instance, async_lib):
-        await self.tod.write(value=str(datetime.datetime.now()))
+    @time_of_day.scan(period=1)
+    async def time_of_day(self, instance, async_lib):
+        """Update time-of-day."""
+        await self.time_of_day.write(value=str(datetime.datetime.now()))
 
     heartbeat = pvproperty(
         value=0,
@@ -87,33 +202,22 @@ class StatusHelper(PVGroup):
         await async_lib.library.sleep(3)
         await self.start_count.write(value=self.start_count.value + 1)
 
-    sysreset = pvproperty(
-        name='SYSRESET',
-        record='sub',
-        doc='IOC Restart',
+    # TODO: any way we can find this information?
+    ca_client_count = pvproperty(
+        value=0,
+        name='CA_CLNT_CNT',
+        record='longin',
+        read_only=True,
+        doc='Number of CA clients [not implemented]',
     )
 
-    @sysreset.putter
-    async def sysreset(self, instance, value):
-        if value == 1:
-            sys.exit(0)
-
-    # TODO: any way we can find this information?
-    # ca_client_count = pvproperty(
-    #     value=0,
-    #     name='CA_CLNT_CNT',
-    #     record='longin',
-    #     read_only=True,
-    #     doc='Number of CA clients',
-    # )
-
-    # ca_connection_count = pvproperty(
-    #     value=0,
-    #     name='CA_CONN_CNT',
-    #     record='longin',
-    #     read_only=True,
-    #     doc='Number of CA Connections',
-    # )
+    ca_connection_count = pvproperty(
+        value=0,
+        name='CA_CONN_CNT',
+        record='longin',
+        read_only=True,
+        doc='Number of CA Connections [not implemented]',
+    )
 
     record_count = pvproperty(
         name='RECORD_CNT',
@@ -181,6 +285,7 @@ class StatusHelper(PVGroup):
         record='ai',
         units='byte',
         read_only=True,
+        doc='Memory used.',
     )
 
     mem_free = pvproperty(
@@ -189,6 +294,7 @@ class StatusHelper(PVGroup):
         record='ai',
         units='byte',
         read_only=True,
+        doc='Memory free.',
     )
 
     mem_max = pvproperty(
@@ -208,92 +314,6 @@ class StatusHelper(PVGroup):
         read_only=True,
     )
 
-    hostname = pvproperty(
-        value='',
-        name='HOSTNAME',
-        record='stringin',
-        read_only=True,
-    )
-
-    app_dir = pvproperty(
-        value='',
-        name='APP_DIR',
-        record='waveform',
-        max_length=255,
-        read_only=True,
-        doc='Script directory',
-    )
-
-    # The following only update once:
-    engineer = pvproperty(
-        value='',
-        name='ENGINEER',
-        record='stringin',
-        read_only=True,
-    )
-
-    location = pvproperty(
-        value='',
-        name='LOCATION',
-        record='stringin',
-        read_only=True,
-    )
-
-    process_id = pvproperty(
-        value=0,
-        name='PROCESS_ID',
-        record='ai',
-        read_only=True,
-    )
-
-    parent_pid = pvproperty(
-        name='PARENT_ID',
-        record='ai',
-        doc="Parent Process ID",
-        read_only=True,
-    )
-
-    cpu_count = pvproperty(
-        value=0,
-        name='CPU_CNT',
-        record='longin',
-        read_only=True,
-        doc='Number of CPUs',
-    )
-
-    kernel_version = pvproperty(
-        value='',
-        name='KERNEL_VERS',
-        record='stringin',
-        doc='OS/Kernel Version',
-        read_only=True,
-    )
-
-    version = pvproperty(
-        value=f'{__version__} on {sys.version}',
-        name='EPICS_VERSION',
-        record='stringin',
-        doc='EPICS (caproto) version',
-        read_only=True,
-    )
-
-    async def _update_one_time_only_values(self):
-        await self.process_id.write(os.getpid())
-        await self.parent_pid.write(os.getppid())
-        await self.location.write(os.environ.get('LOCATION', ''))
-        await self.engineer.write(os.environ.get('ENGINEER', ''))
-        await self.kernel_version.write(platform.uname().version)
-
-        try:
-            # This is standard library, but I'm unsure if it's included on
-            # windows:
-            import resource  # noqa
-            soft_limit, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
-        except OSError:
-            ...
-        else:
-            await self.fd_max.write(soft_limit)
-
     update_period = pvproperty(
         value=15.0,
         name='UPD_TIME',
@@ -303,26 +323,18 @@ class StatusHelper(PVGroup):
         doc='Basic stats update rate',
     )
 
+    async def _update_psutil_status(self):
+        memory_info = self._process.memory_info()
+        await self.mem_used.write(value=memory_info.rss)
+
     async def _update_status_periodic(self):
-        import os
-        try:
-            import psutil
-        except ImportError:
-            self.log.warning(
-                "The Python library psutil is not installed, so MEM_USED will "
-                "not be reported by StatsHelper.")
-            process = psutil.Process(os.getpid())
-            skip_mem_used = True
-        else:
-            skip_mem_used = False
-        if self._root_pvgroup is not None:
-            await self.record_count.write(value=len(self._root_pvgroup.pvdb))
-        if not skip_mem_used:
-            await self.mem_used.write(value=process.memory_info().rss)
+        await self.record_count.write(value=len(self._root_pvgroup.pvdb))
+
+        if self._process is not None:
+            await self._update_psutil_status()
 
     @update_period.startup
     async def update_period(self, instance, async_lib):
-        await self._update_one_time_only_values()
         while True:
             try:
                 await self._update_status_periodic()
@@ -331,16 +343,44 @@ class StatusHelper(PVGroup):
             await async_lib.library.sleep(self.update_period.value)
 
 
+TRACEMALLOC_FILTERS = (
+    tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+    tracemalloc.Filter(False, "<unknown>"),
+    tracemalloc.Filter(False, "<tracemalloc>"),
+)
+
+
 def get_top_allocation_info(snapshot: tracemalloc.Snapshot, *,
                             key_type: str = 'lineno',
-                            limit: int = 10
-                            ) -> typing.List[str]:
-    snapshot = snapshot.filter_traces((
-        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
-        tracemalloc.Filter(False, "<unknown>"),
-        tracemalloc.Filter(False, "<tracemalloc>"),
-    ))
-    top_stats = snapshot.statistics(key_type)
+                            cumulative: bool = False,
+                            limit: int = 10,
+                            filters: Optional[List[tracemalloc.Filter]] = None,
+                            ) -> List[str]:
+    """
+    Get the top allocations from a given tracemalloc snapshot in a list.
+
+    Parameters
+    ----------
+    snapshot : tracemalloc.Snapshot
+        Snapshot to get information from.
+    key_type : str, optional
+        Key for the snapshot statistics.
+    cumulative : bool, optional
+        Cumulative statistics.
+    limit : int, optional
+        Limit the number of results.
+    filters : list of tracemalloc.Filter
+        Filters to apply to the snapshot.
+
+    Returns
+    -------
+    text_lines : list of str
+    """
+    if filters is None:
+        filters = TRACEMALLOC_FILTERS
+
+    snapshot = snapshot.filter_traces(filters)
+    top_stats = snapshot.statistics(key_type, cumulative=cumulative)
 
     result = []
     for index, stat in enumerate(top_stats[:limit], 1):
@@ -356,21 +396,20 @@ def get_top_allocation_info(snapshot: tracemalloc.Snapshot, *,
     return result, top_stats
 
 
-class TracingHelper(PVGroup):
+class MemoryTracingHelper(PVGroup):
+    """
+    A helper which quickly allows for tracing memory usage and allocations on a
+    caproto server instance.
+    """
+    _old_snapshot: tracemalloc.Snapshot
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._root_pvgroup = self._find_top_level_pvgroup()
         self._old_snapshot = None
-
-    def _find_top_level_pvgroup(self):
-        ancestor = self.parent
-        while ancestor.parent is not None:
-            ancestor = ancestor.parent
-        return ancestor
 
     enable_tracing = pvproperty(
         name='EnableTracing',
-        # value='Disable',
+        # value='Disable',  # TODO
         value='Enable',
         doc='Enable/disable in-depth memory analysis',
         record='bo',
@@ -378,12 +417,12 @@ class TracingHelper(PVGroup):
         dtype=ChannelType.ENUM,
     )
 
-    trace_diff_results = pvproperty(
+    diff_results = pvproperty(
         name='TraceDiffResults',
         value='Unset',
         max_length=20000,
         read_only=True,
-        doc='',
+        doc='Trace diff from snapshot to snapshot',
         record='waveform',
     )
 
@@ -392,7 +431,7 @@ class TracingHelper(PVGroup):
         value='Unset',
         max_length=20000,
         read_only=True,
-        doc='',
+        doc='Top allocations in snapshot',
         record='waveform',
     )
 
@@ -414,7 +453,6 @@ class TracingHelper(PVGroup):
         await self.top_allocations.write(
             '\n'.join(top_lines)[:self.top_allocations.max_length]
         )
-        await self.mem_used.write(sum(stat.size for stat in stats))
 
         if self._old_snapshot is not None:
             comparison = snapshot.compare_to(self._old_snapshot, 'lineno')
@@ -424,7 +462,18 @@ class TracingHelper(PVGroup):
                 self.log.debug(stat)
 
             status = '\n'.join(str(stat) for stat in comparison[:10])
-            await self.trace_diff_results.write(
-                status[:self.trace_diff_results.max_length])
+            await self.diff_results.write(
+                status[:self.diff_results.max_length])
 
         self._old_snapshot = snapshot
+
+
+class StatusHelper(PVGroup):
+    """
+    An IocStats-like tool for caproto IOCs.
+
+    Includes all PVs from :class:`BasicStatusHelper` and
+    :class:`PeriodicStatusHelper`.
+    """
+    basic = SubGroup(BasicStatusHelper, prefix='')
+    periodic = SubGroup(PeriodicStatusHelper, prefix='')
