224a96d4e33c4a1d1a0eaf4fad83161ace3bb91f
K Lauer <klauer@slac.stanford.edu>
2020-10-02 16:26:19 -0700

    MNT: a few tweaks before merging

--- caproto/ioc_examples/mirror.py
+++ caproto/ioc_examples/mirror.py
@@ -7,11 +7,10 @@ class Mirror(PVGroup):
     """
     Subscribe to a PV and serve its value.
 
-    The default prefix is `mirror:`
+    The default prefix is ``mirror:``.
 
     PVs
     ---
-
     value
     """
     # TODO This assumes the type of the value is float.
@@ -25,22 +24,32 @@ class Mirror(PVGroup):
         super().__init__(*args, **kwargs)
 
     async def _callback(self, pv, response):
-        await self.value.write(response.data)
+        # Update our own value based on the monitored one:
+        await self.value.write(
+            response.data,
+            # We can even make the timestamp the same:
+            timestamp=response.metadata.timestamp,
+        )
 
     @value.startup
     async def value(self, instance, async_lib):
-        # Subscribe to the target PV and register self._callback.
+        # Note that the asyncio context must be created here so that it knows
+        # which asyncio loop to use:
         self.client_context = Context()
+
         self.pv, = await self.client_context.get_pvs(self.target)
-        self.subscription = self.pv.subscribe()
+
+        # Subscribe to the target PV and register self._callback.
+        self.subscription = self.pv.subscribe(data_type='time')
         self.subscription.add_callback(self._callback)
 
 
 if __name__ == '__main__':
     parser, split_args = template_arg_parser(
         default_prefix='mirror:',
-        desc='Mirror the value of another PV.',
-        supported_async_libs=('asyncio',))
+        desc='Mirror the value of another floating-point PV.',
+        supported_async_libs=('asyncio',)
+    )
     parser.add_argument('--target',
                         help='The PV to mirror', required=True, type=str)
     args = parser.parse_args()
