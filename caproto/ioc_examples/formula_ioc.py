93417b3e6fb410ece8b745c84224292b168e5843
K Lauer <klauer@slac.stanford.edu>
2020-10-14 12:51:39 -0700

    MNT: clean and rebase on top of current master

--- caproto/ioc_examples/formula_ioc.py
+++ caproto/ioc_examples/formula_ioc.py
@@ -1,26 +1,15 @@
 #!/use/bin/env python3
-from caproto.server import pvproperty, PVGroup, template_arg_parser, run
-from caproto import ChannelType
-import numpy as np
-from textwrap import dedent
+
 import ast
+from textwrap import dedent
 
-np_names = set(np.__all__)
-np_ns = {k: getattr(np, k) for k in np_names}
+import numpy as np
 
+from caproto import ChannelType
+from caproto.server import PVGroup, pvproperty, run, template_arg_parser
 
-def extract_names(code_string):
-    output_name, _, expr = map(lambda x: x.strip(),
-                               code_string.partition('='))
-
-    ast_node = ast.parse(expr)
-    code_object = compile(expr, '', 'eval')
-    return (output_name,
-            expr,
-            code_object,
-            set(node.id for node in ast.walk(ast_node)
-                if (isinstance(node, ast.Name) and
-                    node.id not in np_names)))
+NP_NAMES = frozenset(np.__all__)
+NUMPY_NAMESPACE = {k: getattr(np, k) for k in NP_NAMES}
 
 
 class FormulaIOC(PVGroup):
@@ -39,9 +28,8 @@ class FormulaIOC(PVGroup):
     the form `{prefix}{var}`.  The output will be published to
     `{prefix}{ret_name}`
 
-
-    Readonly PVs
-    ------------
+    Read-only PVs
+    -------------
     {prefix}formula -> the equation computed
 
     {prefix}out_postfix -> the PV post-fix where the output is available
@@ -52,13 +40,12 @@ class FormulaIOC(PVGroup):
     Input PVs
     ---------
     {prefix}{var} -> the value for {var} in the inputs
+
+    See Also
+    --------
+    `caproto.server.pvfunction`
     """
     proc = pvproperty(value=0, alarm_group='formula')
-
-    def __init__(self, *args, code_object, var_names, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.code_object = code_object
-        self.var_names = var_names
 
     @proc.putter
     async def proc(self, instance, value):
@@ -70,8 +57,97 @@ class FormulaIOC(PVGroup):
     async def compute(self, instance):
         locals_ = {n: self.pvdb[f'{self.prefix}{n}'].value
                    for n in self.var_names}
-        ret = eval(self.code_object, locals_, np_ns)
+        ret = eval(self.code_object, locals_, NUMPY_NAMESPACE)
         return ret
+
+
+def create_ioc(formula: str) -> type:
+    """
+    Create an IOC based on the given formula.
+
+    Parameters
+    ----------
+    formula : str
+        The formula to create PVs for.
+
+    Returns
+    -------
+    cls : type
+        Subclass of `FormulaIOC`.
+    """
+    # parse the input formula
+    out_name, expr, code_object, names = extract_names(formula)
+
+    # build the pvpropriety objects for then inputs to the formula
+    class_dict = {
+        n: pvproperty(value=0.0, alarm_group='formula')
+        for n in names
+    }
+
+    # build the pvproperty for the output.
+    out_pv = pvproperty(
+        value=0.0,
+        name=out_name,
+        read_only=True,
+        get=FormulaIOC.compute, record='ai',
+        alarm_group='formula'
+    )
+
+    @out_pv.scan(period=.1, use_scan_field=True)
+    async def out_pv(self, instance, async_lib):
+        ret = await self.compute(instance)
+        await instance.write(ret)
+
+    class_dict.update({
+        # Add read-only informational PVs:
+        'output': out_pv,
+        'formula': pvproperty(value=f'{out_name} = {expr}', read_only=True,
+                              report_as_string=True),
+        'out_postfix': pvproperty(value=[out_name], read_only=True,
+                                  dtype=ChannelType.STRING),
+        'vars': pvproperty(value=sorted(names), read_only=True,
+                           dtype=ChannelType.STRING),
+
+        # And information on the formula:
+        'var_names': names,
+        'code_object': code_object,
+    })
+
+    # Dynamically sub-class the FormulaIOC to add the pvproperty
+    # objects we built up above.
+    return type('CustomFormulaIOC', (FormulaIOC,), class_dict)
+
+
+def extract_names(code_string):
+    """
+    Extract variable names from a given string of code.
+
+    Returns
+    -------
+    output_name : str
+        The output variable name.
+
+    expressino : str
+        The formula expression.
+
+    code_object : code
+        The compiled code object.
+
+    names : set of str
+        Node / variable names.
+    """
+    output_name, _, expr = map(lambda x: x.strip(),
+                               code_string.partition('='))
+
+    ast_node = ast.parse(expr)
+    code_object = compile(expr, '', 'eval')
+    return (
+        output_name,
+        expr,
+        code_object,
+        set(node.id for node in ast.walk(ast_node)
+            if isinstance(node, ast.Name) and node.id not in NP_NAMES)
+    )
 
 
 if __name__ == '__main__':
@@ -79,53 +155,17 @@ if __name__ == '__main__':
         default_prefix='Formula:',
         desc=dedent(FormulaIOC.__doc__)
     )
-    # add our own CLI arguments
-    parser.add_argument('--formula',
-                        help=('The formula to evaluate.  ' +
-                              'Must be a python expression.'),
-                        required=True, type=str)
+
+    # add our own CLI argument
+    parser.add_argument(
+        '--formula',
+        help='The formula to evaluate.  Must be a python expression.',
+        required=True,
+        type=str,
+    )
 
     args = parser.parse_args()
     ioc_options, run_options = split_args(args)
 
-    # parse the input formula
-    out_name, expr, code_object, names = extract_names(args.formula)
-
-    # build the pvpropriety objects for then inputs to the formula
-    dyn_pvs = {n: pvproperty(value=0.0, alarm_group='formula')
-               for n in names}
-
-    # build the pvproperty for the output.
-    out_pv = pvproperty(
-        value=0.0,
-        name=out_name, read_only=True,
-        get=FormulaIOC.compute, mock_record='ai',
-        alarm_group='formula')
-
-    @out_pv.scan(period=.1, use_scan_field=True)
-    async def out_pv(self, instance, async_lib):
-        ret = await self.compute(instance)
-        await instance.write(ret)
-
-    dyn_pvs['output'] = out_pv
-
-    # add read-only informational PVs
-    dyn_pvs['formula'] = pvproperty(
-        value=f'{out_name} = {expr}',
-        read_only=True)
-    dyn_pvs['out_postfix'] = pvproperty(
-        value=[out_name], read_only=True, dtype=ChannelType.STRING)
-    dyn_pvs['vars'] = pvproperty(
-        value=sorted(names), read_only=True, dtype=ChannelType.STRING)
-
-    # dynamically sub-class the FormulaIOC to add the pvproperty
-    # objects we built up above.
-    MyIOC = type('MyIOC', (FormulaIOC,), dyn_pvs)
-
-    # instantiate the IOC
-    ioc = MyIOC(code_object=code_object,
-                var_names=names,
-                **ioc_options)
-
-    # and run it!
+    ioc = create_ioc(args.formula)(**ioc_options)
     run(ioc.pvdb, **run_options)
