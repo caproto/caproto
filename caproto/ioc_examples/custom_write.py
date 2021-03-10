ffe84f3924e0ce506fa64d22a9e9f752edd5e6d3
Ken Lauer <klauer@slac.stanford.edu>
2021-03-10 13:55:52 -0800

    MNT: custom_write directory pv

--- caproto/ioc_examples/custom_write.py
+++ caproto/ioc_examples/custom_write.py
@@ -10,17 +10,24 @@ class CustomWrite(PVGroup):
     """
     When a PV is written to, write the new value into a file as a string.
     """
-    DIRECTORY = pathlib.Path(tempfile.gettempdir())
 
     async def my_write(self, instance, value):
         # Compose the filename based on whichever PV this is.
         # For this IOC, the following will be: 'A' or 'B'
-        filename = self.DIRECTORY / instance.name[-1]
+        filename = pathlib.Path(self.directory.value) / instance.name[-1]
         with open(filename, 'wt') as f:
             print(f"{value}", file=f)
 
         self.log.warning(f'Wrote {value} to {filename}')
         return value
+
+    directory = pvproperty(
+        value=tempfile.gettempdir(),
+        doc="The directory to write data to",
+        string_encoding='utf-8',
+        report_as_string=True,
+        max_length=255,
+    )
 
     A = pvproperty(put=my_write, value=0, doc="Writes to (DIRECTORY)/A")
     B = pvproperty(put=my_write, value=0, doc="Writes to (DIRECTORY)/B")
