6a168fcf5d20abbc2ebf577b322c6ec6303903a3
Ken Lauer <klauer@slac.stanford.edu>
2021-03-10 11:57:03 -0800

    MNT: add more details to ioc_all_in_one

--- caproto/tests/ioc_all_in_one.py
+++ caproto/tests/ioc_all_in_one.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+"""
+This was an original test of the style of how to write a PVGroup.  It's not
+an example that you should use to base your IOCs on.
+"""
 import logging
+from textwrap import dedent
 
 from caproto.server import PVGroup, ioc_arg_parser, pvproperty, run
 
@@ -7,13 +12,26 @@ logger = logging.getLogger('caproto')
 
 
 class MyPVGroup(PVGroup):
-    'Example group of PVs, where the prefix is defined on instantiation'
+    """
+    Example group of PVs.
+
+    * "single" is defined in the "old" property style, where get/put
+       hooks can be re-used.
+    * "testa" - shares the getter read_int
+    * "testb" - shares the getter read_int
+    * "testc" - shares the getter read_int
+    * "macro" - uses the user-defined macro for its PV name
+    * "newstyle" - uses "new style" - in Python terminology - properties
+      with decorators to define putters
+    * "random" - uses a "new style" property, defaulting to an int
+    """
     # starting off with something like old-style property-like PVs:
 
     # PV #1: {prefix}single
     # - has custom read/write methods defined in the wrapped function
     async def single_read(self, instance):
         # NOTE: self is the group, instance is the channeldata instance
+        # NOTE: please do not use getters; see the docs for more details.
         logger.debug('read single')
         return ['a']
 
@@ -25,14 +43,15 @@ class MyPVGroup(PVGroup):
 
     async def read_int(self, instance):
         logger.debug('read_int of %s', instance.pvspec.attr)
+        # NOTE: please do not use getters; see the docs for more details.
         return instance.value
 
     # a set of 3 PVs re-using the same spec-func
     # PV #2: {prefix}testa
     testa = pvproperty(read_int, value=1)
-    # PV #3: {prefix}testa
+    # PV #3: {prefix}testb
     testb = pvproperty(read_int, value=2)
-    # PV #4: {prefix}testa
+    # PV #4: {prefix}testc
     testc = pvproperty(read_int, value=3)
 
     # PV #5: {prefix}{macro}
@@ -61,7 +80,7 @@ class MyPVGroup(PVGroup):
 if __name__ == '__main__':
     ioc_options, run_options = ioc_arg_parser(
         default_prefix='all_in_one:',
-        desc='Run an IOC that is an amalgam of several other examples.',
+        desc=dedent(MyPVGroup.__doc__),
         macros=dict(macro='expanded'))
     ioc = MyPVGroup(**ioc_options)
 
