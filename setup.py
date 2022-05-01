e32f187dfe0178ea24822d624cf93fbd19a6c95b
Thomas A Caswell <tcaswell@bnl.gov>
2022-04-30 20:53:21 -0400

    API: remove support for py36

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import versioneer
 # NOTE: This file must remain Python 2 compatible for the foreseeable future,
 # to ensure that we error out properly for people with outdated setuptools
 # and/or pip.
-if sys.version_info < (3, 6):
+if sys.version_info < (3, 7):
     error = """
 Caproto does not support Python 2.x, 3.0, 3.1, 3.2, 3.3, 3.4, or 3.5.
 Python 3.6 and above is required. Check your Python version like so:
@@ -29,7 +29,6 @@ classifiers = [
     'Intended Audience :: Science/Research',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
@@ -81,7 +80,7 @@ setup(name='caproto',
           # rebuild record fields:
           '': ['*.rst', '*.jinja2'],
       },
-      python_requires='>=3.6',
+      python_requires='>=3.7',
       classifiers=classifiers,
       extras_require=extras_require
       )
