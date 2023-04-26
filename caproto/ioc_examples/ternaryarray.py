f40a39649e6ac0f774160805a2a5ddc5d23c3193
gbischof <bisc8233@gmail.com>
2023-04-26 15:58:13 -0400

    add dynamic putters

--- caproto/ioc_examples/ternaryarray.py
+++ caproto/ioc_examples/ternaryarray.py
@@ -2,6 +2,8 @@ import asyncio
 import threading
 import time
 
+from functools import partialmethod
+
 from caproto.server import PVGroup, ioc_arg_parser, pvproperty, run
 from ophyd import EpicsSignal, EpicsSignalRO, PVPositionerPC
 from ophyd import Component as Cpt
@@ -52,26 +54,35 @@ class TernaryArrayIOC(PVGroup):
         super().__init__(*args, **kwargs)
         self._count = count
         self._devices = [TernaryDevice() for i in range(count)]
+
+        # Create pvs for all of the devices.
         for i in range(count):
             setattr(self, f'device{i}', pvproperty(value=0, dtype=int, name=f'device{i}'))
             setattr(self, f'device{i}_rbv', pvproperty(value=0, dtype=int, name=f'device{i}_rbv'))
+
+        # Assign putters of all of the setpoint pvs.
+        for i in range(count):
+            getattr(self, f'device{i}').putter(partialmethod(general_putter, i))
+
+    async def general_putter(self, index, instance, value):
+        if value:
+            self.devices[index].set()
+        else:
+            self.devices[index].reset()
 
     async def device_poller(self):
         while True:
             for i in range(self.count):
                 await getattr(self, f'device{i}_rbv').write(self._devices[i].state)
 
-    @Kp.putter
-    async def Kp(self, instance, value):
-        self._temperature_controller.Kp = value
-        return value
 
-
+"""
 if __name__ == "__main__":
     ioc_options, run_options = ioc_arg_parser(
-        default_prefix="TernaryArray:", desc="Lakeshore IOC"
+        default_prefix="TernaryArray:", desc="TernaryArray IOC"
     )
     ioc = TernaryArrayIOC(**ioc_options)
 
     print("PVs:", list(ioc.pvdb))
     run(ioc.pvdb, **run_options)
+"""
